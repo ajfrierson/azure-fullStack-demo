c283e4142445626cc0f20cb99be2038ff0775e85
Alvin <huerox@msn.com>
2022-03-24 07:32:08 -0700

    initial setup

---
+++ src/main/java/com/example/azurefullStackdemo/AzureFullStackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.azurefullStackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureFullStackDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureFullStackDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azurefullStackdemo/AzureFullStackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurefullStackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureFullStackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
